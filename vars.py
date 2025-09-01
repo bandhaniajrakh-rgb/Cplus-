f584e138bb28bd40c62d1a518d87cda71e3a8038
bandhaniajrakh-rgb <bandhaniajrakh@gmail.com>
2025-09-02 02:22:51 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,7 +1,8 @@
-api_id = 35814071 #change_with_your_Api_id.
-api_hash = "80c231f0e8704995b8259d06965n4351" #change_with_your_Api_hash.
-bot_token = "7198600769:AAHfVi7qTaMw20HbpvPrF-fnP77-sWdOthI" #change_with_your_bot_token.
-auth_users = [999741495] #change_with_your_user_id.
-sudo_user = 1713924419 #change_with_your_user_id.
-log_channel = -1002154281532 #change_with_your_channel_id.
-txt_channel = -1002154281532 #change_with_your_channel_id.
+api_id = 21546266 #change_with_your_Api_id.
+api_hash = "566bbdbeac565e01e74dc835ddb926ac" #change_with_your_Api_hash.
+bot_token = "8465253410:AAFeqZOgrXNfQpwDcZeUhjjIgYm8dYVYegI" #change_with_your_bot_token.
+auth_users = [798617198] #change_with_your_user_id.
+sudo_user = 798617198 #change_with_your_user_id.
+log_channel = -1002938056549 #change_with_your_channel_id.
+txt_channel = -1002938056549 #change_with_your_channel_id.
+
