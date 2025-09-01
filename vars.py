27113d0a0606ba8cccb46a10c926ca40f7b35257
bandhaniajrakh-rgb <bandhaniajrakh@gmail.com>
2025-09-02 02:36:30 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,8 +1,9 @@
 api_id = 21546266 #change_with_your_Api_id.
 api_hash = "566bbdbeac565e01e74dc835ddb926ac" #change_with_your_Api_hash.
-bot_token = "8465253410:AAFeqZOgrXNfQpwDcZeUhjjIgYm8dYVYegI" #change_with_your_bot_token.
+bot_token = "8476065377:AAGRLCh4uHCIEKqp4bm5_HgsKutVwLOD7o8" #change_with_your_bot_token.
 auth_users = [798617198] #change_with_your_user_id.
 sudo_user = 798617198 #change_with_your_user_id.
 log_channel = -1002938056549 #change_with_your_channel_id.
 txt_channel = -1002938056549 #change_with_your_channel_id.
+
 
